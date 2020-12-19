4562e64ad38e4cf25d888f28ecc6d3666d8c1d34
Adi Dahari <dahariadi16@gmail.com>
2020-12-19 16:25:21 +0200

    Ex2 jar added to main folder

--- src/Ex2.java
+++ src/Ex2.java
@@ -14,6 +14,27 @@ import java.awt.image.BufferedImage;
 import java.util.*;
 import java.util.List;
 
+/**
+ * This class is the main class of the Pokemon Game.
+ * each game is implemented as a thread including a GUI and run method.
+ * the initialization of the game is supporting 2 options:
+ * 1. log in through a graphic login page with scenario number (0-23) and id number.
+ * 2. log in through command prompt by the command: java -jar Ex2.jar [*id number*] [*scenario number*]
+ * as Agents' speed is changing according to their value, for preventing useless moves the sleep time of the game's thread is changing through time.
+ * class structure:
+ * 1. paths = HashMap containing each agent's current path as a list of node_data, for optimizing the agents' collaboration.
+ * 2. dests = HashMap containing each agent's current destination pokemon, for preventing 2 agents going after the same pokemon together.
+ * 3.a. _id = i.d number for logging in to ther server. b. _scNum = the scenario of the current run of the game [0 - 23]
+ * 4. _gui = GUI for showing the game on screen as it runs.
+ * 5. _ar = Arena to maintain the game elements as the game runs.
+ * class methods:
+ * 1. run = the thread maintenance for the game, controls the sleep time and the communication with the provided game server.
+ * 2. moveAgents = handling the Agents movements as the game runs.
+ * 3. nextNode = the main decision making algorithm for each agent. considers the current pokemons and agents locations and each agent's path.
+ *               each agent next node is selected by this method according to all relevant parameters.
+ * init = initializing the game's arena and by using a json parsing functions. builds the graph according to the server information for each scenario by using the classes implemented in the api package.
+ * Login (class) = the implementation of the GUI for the login page of the game. using JTextField and a graphic button with ActionListener - implemented using lambda expressions.
+ */
 public class Ex2 implements Runnable {
     private static HashMap<Integer, List<node_data>> paths;
     private static HashMap<Integer, CL_Pokemon> dests;
@@ -21,6 +42,12 @@ public class Ex2 implements Runnable {
     private static int _scNum;
     private static GUI _gui;
     private static Arena _ar;
+
+    /**
+     * The main of the game, handles the login information and initializing the GUI.
+     * if no information provided from cmd it calls the login page and gets the needed info from it.
+     * @param a = String array for the login information. the way to login with cmd is: java -jar Ex2.jar [*id*] [*scenario*]
+     */
     public static void main(String[] a) {
         if(a.length != 2) {
             _gui = new GUI("Login Page", 350,300);
@@ -39,6 +66,10 @@ public class Ex2 implements Runnable {
         }
     }
 
+    /**
+     * this is the thread's control element.
+     * decides when the thread sleeps and maintaining connection with the server.
+     */
     @Override
     public void run() {
         game_service game = Game_Server_Ex2.getServer(_scNum); // you have [0,23] games
@@ -91,11 +122,10 @@ public class Ex2 implements Runnable {
         System.exit(0);
     }
     /**
-     * Moves each of the agents along the edge,
-     * in case the agent is on a node the next destination (next edge) is chosen (randomly).
-     * @param game
-     * @param gg
-     * @param
+     * this is the agents controller, handles the movement of the agents through the graph.
+     * using nextNode method for getting the next movement for each agent.
+     * @param game = the game service for communication with the server
+     * @param gg = the graph of the current scenario
      */
     private static void moveAgants(game_service game, directed_weighted_graph gg) {
         String lg = game.move();
@@ -120,6 +150,18 @@ public class Ex2 implements Runnable {
         }
     }
 
+    /**
+     * This is the main method of the game's idea.
+     * it chooses for each agent the right node to go to, according to the position of all agents and pokemons.
+     * as it gets the agent's key, the graph and the current node of the agents, it uses the methods maid in api class.
+     * the given graph is initialized in graph algorithms object for performing checks according to each path for the agent
+     * the main idea of this method is to point the agent to the best movement possible for achieving best value.
+     * for each pokemon on current position if the game, it computes the closest agent to it and adressing the agent to the closest pokemon.
+     * @param g = the graph of the current scenario.
+     * @param src = start node's key.
+     * @param agKey = agent's key (or ID).
+     * @return int = the best next node for the agent to go to.
+     */
     private static int nextNode(directed_weighted_graph g, int src, int agKey) {
         dw_graph_algorithms ga = new DWGraph_Algo(g);
         int ans = -1;
@@ -162,6 +204,13 @@ public class Ex2 implements Runnable {
         else return minSrc;
     }
 
+    /**
+     * The initialization of the game.
+     * as it gets all information needed as JSON strings,
+     * it sets a new arena using this information.
+     * for the most efficient initialization, the pokemons are sorted by their values and the agents are placed according to the best pokemons (relatively to their value).
+     * @param game = game service of the current scenario selected.
+     */
     private void init(game_service game) {
         String fs = game.getPokemons();
         String g = game.getGraph();
@@ -198,19 +247,12 @@ public class Ex2 implements Runnable {
                 int ind = a%cl_fs.size();
                 CL_Pokemon c = cl_fs.get(ind);
                 int nn = c.getSrcNode();
-//                if(c.getType()<0 ) {nn = c.get_edge().getSrc();}
-
                 game.addAgent(nn);
             }
         }
         catch (JSONException e) {e.printStackTrace();}
     }
-    private static boolean isOnPath(CL_Pokemon p, List<node_data> path, directed_weighted_graph g){
-        for(int i = 0; i < path.size()-2; i++){
-            if(p.get_edge() == g.getEdge(path.get(i).getKey(), path.get(i+1).getKey())) return true;
-        }
-        return false;
-    }
+
 
 
 
@@ -221,6 +263,11 @@ public class Ex2 implements Runnable {
      */
     public static class Login extends JPanel {
         private ImageIcon bg = new ImageIcon("gui/background.jpg");
+
+        /**
+         * paintComponenets is extended to have a nice background for the login page.
+         * @param g = Graphics
+         */
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
@@ -229,6 +276,10 @@ public class Ex2 implements Runnable {
 
         }
 
+        /**
+         * this is the implementation of the login page, using the GUI class created for graphic interface.
+         *
+         */
         public Login(){
 
             super();
@@ -262,14 +313,13 @@ public class Ex2 implements Runnable {
             start.setBackground(Color.green.darker().darker());
             start.setBorder(BorderFactory.createLineBorder(Color.black,2));
             add(start);
-            start.addActionListener(e ->{
+            start.addActionListener(e ->{    //lambda expression for performing the initialization of the game according rhe provided id and scenario number.
                 String id = idIn.getText();
                 String sc = scIn.getText();
                 String args[] = {id,sc};
                 main(args);
             });
-            start.addActionListener(e ->{ final int scNumIn = Integer.parseInt(scIn.getText());});
-            start.addActionListener(e -> _gui.setVisible(false));
+            start.addActionListener(e -> _gui.setVisible(false));  //closing the login page frame as it no longer necessary.
         }
     }
 
